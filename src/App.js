4f7514e67c283b4541724e3029807c8b94a34f7c
nati589 <natimok55@gmail.com>
2023-08-05 05:54:21 -0900

    Course cards

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import BooksPage from "./pages/BooksPage";
-// import CoursesPage from "./pages/CoursesPage";
-import Course from "./pages/Course";
 import CourseDetailsPage from "./pages/CourseDetailsPage";
-// import CourseList from "./components/CourseList";
-// import CoursesHero from "./components/CoursesHero.jsx"
-// import OtherCourses from "./components/OtherCourses.jsx"
 import Background from "./components/Background";
 import CoursesPage from "./pages/CoursesPage";
 
@@ -21,16 +16,9 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/books" element={<BooksPage />} />
-        {/* <Route path="/courses" element={<Course />} /> */}
-
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/coursedetails" element={<CourseDetailsPage />} />
       </Routes>
-              
-      
-      {/* <CoursesHero/>
-      <CourseList/>
-      <OtherCourses/> */}
       <Subscription />
       <Footer />
     </div>

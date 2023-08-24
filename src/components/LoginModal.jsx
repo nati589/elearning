a7bc3d7352d36fb091c4a3dce1c36e5ffac8d118
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-08-24 12:33:04 +0300

    Update LoginModal.jsx

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import EduviLogo from '../assets/eduvi_logo.svg'
 import LoginImage from '../assets/login_image.svg'
 import DividerVertical from '../assets/divider_line.svg'
+import InputComponent from './InputComponent'
+import GoogleIcon from '../assets/google_icon.svg'
 
 function LoginModal() {
     
@@ -11,18 +13,20 @@ function LoginModal() {
             <div className='modal-body flex flex-row h-10/12 bg-white fixed w-8/12 rounded-xl p-4'>
                 <div className='flex flex-col flex-nowrap justify-center items-left w-1/2 p-14 pt-10 pr-10 font-sans'>
                     <img src={EduviLogo} alt='eduvi logo' className='w-4/12 mb-5'/>
-                    <span className='font-black leading-normal text-{40px} mb-5'>
+                    <span className='font-black leading-normal text-4xl mb-5'>
                     welcome to <br/> Eduvi online <br/> learning platform
                     </span>
                     <img src={LoginImage} alt='login asset' className='w-8/12 h-auto m-auto'/>
                 </div>
                 <img src={DividerVertical} alt='vertical divider' className='h-auto m-auto'/>
                 <div className='flex flex-col flex-nowrap justify-center items-left w-1/2 p-10 pt-10 font-sans'>
-                    <img src={EduviLogo} alt='eduvi logo' className='w-4/12 mb-5'/>
-                    <span className='font-black leading-18 mb-5'>
-                    welcome to <br/> Eduvi online <br/> learning platform
-                    </span>
-                    <img src={LoginImage} alt='login asset' className='w-8/12 h-auto m-auto'/>
+                    <div className='flex-row font-mono font-thin bg-opacity-10 border-2 h-10 p-2 pr-3 pl-3 mt-2 w-full rounded-md'>
+                        <img src={GoogleIcon} alt='google icon' className='h-auto m-auto'/>
+                    </div>
+                    <InputComponent label='Enter name' placeholder='enter name' type='text'/>
+                    <InputComponent label='Enter name2' placeholder='enter name' type='text'/>
+                    <InputComponent label='Enter name3' placeholder='enter name' type='text'/>
+                    <InputComponent label='Enter name4' placeholder='enter name' type='text'/>
                 </div>
             </div>
         </div>

2b773968178afc8df9b2fc64dedc8598650ab5b2
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-08-25 10:35:36 +0300

    Update SignupModal.jsx

--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -11,40 +11,44 @@ function SignupModal({toggle, toggleSignin}) {
   return (
     <>
     <div  className='modal-overlay h-screen w-full bg-black bg-opacity-60 fixed top-0 bottom-0 left-0 right-0 flex flex-col flex-nowrap justify-center items-center z-50'>
-        <div className='modal-body flex flex-row h-fit bg-white fixed w-8/12 rounded-xl p-4'>
-            <GoXCircle onClick={toggle} className='relative -top-72 right-[-900px] text-edu-purple w-8 h-auto z-50'/>
-            <div className='flex flex-col flex-nowrap justify-center items-left w-1/2 p-8 pl-24 font-sans'>
-                <img src={EduviLogo} alt='eduvi logo' className='w-2/12'/>
-                <span className=' leading-normal font-black text-4xl mb-5'>
-                    Welcome to <br/> EthLang online <br/> learning platform
-                </span>
-                <img src={LoginImage} alt='login asset' className='w-8/12 h-auto m-auto mb-3 mt-3'/>
-            </div>
-            <img src={DividerVertical} alt='vertical divider' className='h-auto m-auto'/>
-            <div className='flex flex-col flex-nowrap justify-center items-center w-1/2 p-8 font-sans'>
-                <a className='flex flex-row justify-center items-center font-thin bg-opacity-10 border-2 h-auto p-2 pr-3 pl-3 mt-2 mb-3 w-auto rounded-lg font-sans text-sm text-gray-500 hover:bg-edu-purple hover:text-white' href='/'>
-                    <img src={GoogleIcon} alt='google icon' className='h-8 m-auto mr-2'/>
-                    Sign Up with Google
-                </a>
-                <div className='flex flex-row flex-nowrap justify-center items-center w-11/12 text-gray-600 font-bold'>
-                   <div className='w-5/12 justify-center items-center px-1 '> <hr className='w-full border-2'/></div>
-                    or
-                   <div className='w-5/12 justify-center items-center px-1'> <hr className='w-full border-2'/></div>
+     
+         <div className='modal-body flex flex-col h-fit bg-white w-8/12 rounded-xl p-4'>
+            <GoXCircle onClick={toggle} className='block self-end text-edu-purple w-8 h-auto'/>
+            <div className='modal-body flex flex-row'>
+                <div className='flex flex-col flex-nowrap justify-center items-left w-1/2 p-8 pl-24 font-sans'>
+                    <img src={EduviLogo} alt='eduvi logo' className='w-2/12'/>
+                    <span className=' leading-normal font-black text-4xl mb-5'>
+                        Welcome to <br/> EthLang online <br/> learning platform
+                    </span>
+                    <img src={LoginImage} alt='login asset' className='w-8/12 h-auto m-auto mb-3 mt-3'/>
                 </div>
-                <InputComponent logo={<GoPerson className='relative top-7 left-3  text-gray-500'/>} label='Full name' placeholder='Enter your full name' type='text'/>
-                <InputComponent logo={<GoMail className='relative top-7 left-3  text-gray-500'/>} label='Email' placeholder='Enter your email' type='email'/>
-                <InputComponent logo={<GoLock className='relative top-7 left-3  text-gray-500'/>} label='Password' placeholder='Enter password' type='password'/>
-                <label className='text-grey-500 text-sm'>
-                    <input type="checkbox"  className='mx-3 text-edu-purple'/>
-                    I agreed to the <a href='/' className='font-bold '>terms and conditions</a>
-                </label>
-                <button className='w-9/12 h-10 my-3 bg-edu-purple hover:bg-edu-purple-hover rounded-md text-white'>
-                    Sign Up
-                </button>
+                <img src={DividerVertical} alt='vertical divider' className='h-auto m-auto'/>
+                <div className='flex flex-col flex-nowrap justify-center items-center w-1/2 p-8 font-sans'>
+                    <a className='flex flex-row justify-center items-center font-thin bg-opacity-10 border-2 h-auto p-2 pr-3 pl-3 mt-2 mb-3 w-auto rounded-lg font-sans text-sm text-gray-500 hover:bg-edu-purple hover:text-white' href='/'>
+                        <img src={GoogleIcon} alt='google icon' className='h-8 m-auto mr-2'/>
+                        Sign Up with Google
+                    </a>
+                    <div className='flex flex-row flex-nowrap justify-center items-center w-11/12 text-gray-600 font-bold'>
+                    <div className='w-5/12 justify-center items-center px-1 '> <hr className='w-full border-2'/></div>
+                        or
+                    <div className='w-5/12 justify-center items-center px-1'> <hr className='w-full border-2'/></div>
+                    </div>
+                    <InputComponent logo={<GoPerson className='relative top-7 left-3  text-gray-500'/>} label='Full name' placeholder='Enter your full name' type='text'/>
+                    <InputComponent logo={<GoMail className='relative top-7 left-3  text-gray-500'/>} label='Email' placeholder='Enter your email' type='email'/>
+                    <InputComponent logo={<GoLock className='relative top-7 left-3  text-gray-500'/>} label='Password' placeholder='Enter password' type='password'/>
+                    <label className='text-grey-500 text-sm'>
+                        <input type="checkbox"  className='mx-3 text-edu-purple'/>
+                        I agreed to the <a href='/' className='font-bold '>terms and conditions</a>
+                    </label>
+                    <button className='w-9/12 h-10 my-3 bg-edu-purple hover:bg-edu-purple-hover rounded-md text-white'>
+                        Sign Up
+                    </button>
 
-                <span className='text-sm text-grey-500 font-sans'>Already have an account? <span onClick={toggleSignin} className='text-edu-purple '>Sign In</span></span>
+                    <span className='text-sm text-grey-500 font-sans'>Already have an account? <span onClick={toggleSignin} className='text-edu-purple '>Sign In</span></span>
+                </div>
             </div>
         </div>
+        
     </div>
    
 </>

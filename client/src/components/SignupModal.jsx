bd0e599ecca6c90e589a1e2c1b3f87abc0e0e7af
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-08-30 15:29:44 +0300

    color updates

--- client/src/components/SignupModal.jsx
+++ client/src/components/SignupModal.jsx
@@ -13,7 +13,7 @@ function SignupModal({toggle, toggleSignin}) {
     <div  className='modal-overlay h-screen w-full bg-black bg-opacity-60 fixed top-0 bottom-0 left-0 right-0 flex flex-col flex-nowrap justify-center items-center z-50'>
      
          <div className='modal-body flex flex-col h-fit bg-white w-8/12 rounded-xl p-4'>
-            <GoXCircle onClick={toggle} className='block self-end text-edu-purple hover:text-edu-purple-hover w-8 h-auto'/>
+            <GoXCircle onClick={toggle} className='block self-end text-medium-purple hover:text-medium-purple-hover w-8 h-auto'/>
             <div className='modal-body flex flex-row'>
                 <div className='flex flex-col flex-nowrap justify-center items-left w-1/2 p-8 pl-24 font-sans'>
                     <img src={EduviLogo} alt='eduvi logo' className='w-2/12'/>
@@ -24,7 +24,7 @@ function SignupModal({toggle, toggleSignin}) {
                 </div>
                 <img src={DividerVertical} alt='vertical divider' className='h-[500px] m-auto'/>
                 <div className='flex flex-col flex-nowrap justify-center items-center w-1/2 p-8 font-sans'>
-                    <a className='flex flex-row justify-center items-center font-thin bg-opacity-10 border-2 h-auto p-2 pr-3 pl-3 mt-2 mb-3 w-auto rounded-lg font-sans text-sm text-gray-500 hover:bg-edu-purple hover:text-white' href='/'>
+                    <a className='flex flex-row justify-center items-center font-thin bg-opacity-10 border-2 h-auto p-2 pr-3 pl-3 mt-2 mb-3 w-auto rounded-lg font-sans text-sm text-gray-500 hover:bg-medium-purple hover:text-white' href='/'>
                         <img src={GoogleIcon} alt='google icon' className='h-8 m-auto mr-2'/>
                         Sign Up with Google
                     </a>
@@ -39,14 +39,14 @@ function SignupModal({toggle, toggleSignin}) {
                         <InputComponent logo={<GoMail className='relative top-7 left-3  text-gray-500'/>} label='Email' placeholder='Enter your email' type='email' required='required'/>
                         <InputComponent logo={<GoLock className='relative top-7 left-3  text-gray-500'/>} label='Password' placeholder='Enter password' type='password' required='required'/>
                         <label className='text-grey-500 text-sm'>
-                            <input type="checkbox"  className='mx-3 text-edu-purple' required/>
+                            <input type="checkbox"  className='mx-3 text-medium-purple' required/>
                             I agreed to the <a href='/' className='font-bold '>terms and conditions</a>
                         </label>
-                        <button className='w-9/12 h-10 my-3 bg-edu-purple hover:bg-edu-purple-hover rounded-md text-white'>
+                        <button className='w-9/12 h-10 my-3 bg-medium-purple hover:bg-medium-purple-hover rounded-md text-white'>
                             Sign Up
                         </button>
                     </form>
-                    <span className='text-sm text-grey-500 font-sans'>Already have an account? <span onClick={toggleSignin} className='text-edu-purple '>Sign In</span></span>
+                    <span className='text-sm text-grey-500 font-sans'>Already have an account? <span onClick={toggleSignin} className='text-medium-purple '>Sign In</span></span>
                 </div>
             </div>
         </div>

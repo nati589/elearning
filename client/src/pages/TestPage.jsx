54ad5f7d83132d4324cad9ac15404f3fb84d551d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-08-30 14:56:57 +0300

    Create TestPage.jsx

---
+++ client/src/pages/TestPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import LandingHero from "../components/LandingHero";
+import OurGoal from "../components/OurGoal";
+import CoursesList from "../components/CourseList";
+import Mentor from "../components/Mentor";
+// import CoursesHero from "../components/CoursesHero";
+
+import Explore from "../components/Explore";
+// import { useNavigate } from "react-router-dom";
+
+function TestPage() {
+  // const navigate = useNavigate();
+  return (
+    <>
+      <LandingHero />
+      <OurGoal />
+      <CoursesList />
+      <Mentor />
+      <Explore />
+    </>
+  );
+}
+
+export default TestPage;

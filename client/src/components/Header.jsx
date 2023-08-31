0c28dac2e6d75b000ce72505a434981bd7f07193
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-08-31 14:09:44 +0300

    Update Header.jsx
    updating header buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,6 +28,7 @@ function Header() {
   const toggleSignin = () => {
     setSigninModal(!signinModal);
     setSignupModal(false);
+    setMobileMenuOpen(false)
 
     !signinModal ? disableScroll() : enableScroll();
   };
@@ -35,6 +36,7 @@ function Header() {
   const toggleSignup = () => {
     setSignupModal(!signupModal);
     setSigninModal(false);
+    setMobileMenuOpen(false)
 
     !signupModal ? disableScroll() : enableScroll();
   };
@@ -49,7 +51,6 @@ function Header() {
       )}
 
       <header className="sticky inset-x-0 top-0 z-30 bg-white">
-        {/* <div className="relative isolate px-6 pt-14 lg:px-8"> */}
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true">
@@ -61,25 +62,24 @@ function Header() {
             }}
           />
         </div>
-        {/* </div> */}
-        <nav
-          className=" text-[#0A033C] flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global">
+        <nav className=" text-[#0A033C] flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+          
           <div className="flex lg:flex-1">
             <a href="  /" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src={logo} alt="" />
+              <img className="h-8 w-auto" src={logo} alt="company logo" />
             </a>
           </div>
+
+
           <div className="flex lg:hidden">
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(true)}>
-              <span className="sr-only">Open main menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
+
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a
@@ -90,34 +90,27 @@ function Header() {
               </a>
             ))}
           </div>
+
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <div
-              onClick={toggleSignin}
-              className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </div>
+            <div onClick={toggleSignin} className="button-component">Log in </div>
+            <div onClick={toggleSignup} className="button-component-stroke">Sign Up</div>
           </div>
         </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}>
+
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+
           <div className="fixed inset-0 z-50" />
+
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
                 <img className="h-8 w-auto" src={logo} alt="" />
               </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}>
-                <span className="sr-only">Close menu</span>
+              <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(false)}>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
+
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
@@ -130,12 +123,12 @@ function Header() {
                     </a>
                   ))}
                 </div>
+
+                {/* mobile nav login */}
+
                 <div className="py-6">
-                  <div
-                    onClick={toggleSignin}
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                    Log in
-                  </div>
+                  <div onClick={toggleSignin} className="button-component my-2">Log in </div>
+                  <div onClick={toggleSignup} className="button-component-stroke my-2">Sign Up</div>
                 </div>
               </div>
             </div>

d2da8bf3d0f4562e4918661bbf09fd6dbce8a1e5
selamawitsiferh <siferhselamawit@gmail.com>
2023-09-01 17:02:40 +0300

    I edit PopularCourceCard.js files and test files

--- client/src/components/PopularCourseCard.jsx
+++ client/src/components/PopularCourseCard.jsx
@@ -9,59 +9,63 @@ import stickynote from"../assets/icons/stickynote.svg";
 
 const PopularCourseCard = ({course_title,course_details,course_thumbnail,course_total_hour,course_instructure,course_level,course_rate,course_price}) => {
   return (
-    <div class="card w-300 border border-gray-300 p-20 m-10 bg-white 70">
-    <h1 class="text-4xl mb-4 font-bold">{course_title}</h1>
+
+  
+<div class="card bg-white border-l-8 border-l-dark-purple rounded-lg shadow-md my-4 p-6">
+<h1 class="text-4xl mb-4 font-bold">{course_title}</h1>
  <div  class="grid grid-cols-4 gap-4"> 
-    <div class="col-span-1"><img  class="1/5" src={course_thumbnail} alt="books" /></div><div class="col-span-3"><p class="text-lg">{course_details}</p></div></div>
+     <div class="col-span-1"><img class="w-60 h-13 " src={course_thumbnail} alt="books" /></div><div class="col-span-3"><p class="text-lg">{course_details}</p></div></div>
    
     <div class="flex flex-row m-10">
-  <div class="flex-grow">
-  <div class="grid grid-rows-3 ">
-  <div class='flex flex-row'>
-  <img  class="w-7"src={timer} alt="" />
-  <p  class="ml-4">{course_total_hour}</p>
+   <div class="flex-grow">
+   <div class="grid grid-rows-3 ">
+   <div class='flex flex-row'>
+   <img  class="w-6 h-6"src={timer} alt="" />
+   <p  class="ml-2">{course_total_hour}</p>
   
-  </div>
-  <div class='flex flex-row'>
-  <img  class="w-7"src={person} alt="" />
-  <p  class="ml-4">{course_instructure}</p>
-  </div>
-  <div class='flex flex-row'>
-  <img  class="w-7"src={stickynote} alt="" />
-  <p  class="ml-4">{course_level}</p>
-  </div>
+   </div>
+   <div class='flex flex-row'>
+   <img  class="w-6 h-6"src={person} alt="" />
+   <p  class="ml-2">{course_instructure}</p>
+   </div>
+   <div class='flex flex-row'>
+   <img  class="w-6 h-6"src={stickynote} alt="" />
+   <p class="ml-2">{course_level}</p>
+   </div>
  
  
-</div>
-  </div>
-  <div class="flex-grow">
-  <div class="grid grid-rows-3 ">
-  <div class='flex flex-row'>
-  <p  class="ml-4">{course_rate}</p>
-  <img  class="w-5"src={fullstar} alt="" />
-  <img  class="w-5"src={fullstar} alt="" />
-  <img  class="w-5"src={fullstar} alt="" />
-  <img  class="w-5"src={fullstar} alt="" />
-  <img  class="w-5"src={fullstar} alt="" />
+ </div>
+   </div>
+   <div class="flex-grow">
+   <div class="grid grid-rows-3 ">
+   <div class='flex flex-row'>
+   <p  class="ml-2">{course_rate}</p>
+   <img  class="w-4 h-4" src={fullstar} alt="" />
+   <img  class="w-4 h-4" src={fullstar} alt="" />
+   <img  class="w-4 h-4" src={fullstar} alt="" />
+   <img  class="w-4 h-4" src={fullstar} alt="" />
+   <img  class="w-4 h-4" src={fullstar} alt="" />
 
 
-  </div>
-  <div class='flex flex-row'>
-  <img  class="w-7"src={dollar} alt="" />
-  <p  class="ml-4">{course_price}</p>
-  </div>
-<div class='flex flex-row'>
-<button class="button-component-stroke"> Add to cart</button>
-  {/* <button class="bg-white-600 hover:bg-blue-800 text-blue font-bold py-2 px-4 rounded border border-purple-500"> Add to cart</button> */}
-  </div>
+   </div>
+   <div class='flex flex-row'>
+   <img  class="w-6 h-6"src={dollar} alt="" />
+   <p  class="ml-2">{course_price}</p>
+   </div>
+ <div class='flex flex-row-m'>
+ <button class="button-component-stroke"> Add to cart</button>
+   {/* <button class="bg-white-600 hover:bg-blue-800 text-blue font-bold py-2 px-4 rounded border border-purple-500"> Add to cart</button> */}
+   </div>
  
  
-</div>
-  </div>
+ </div>
+   </div>
+ </div>
 </div>
 
-    </div>
-  
+
+
+
   )
 }
 

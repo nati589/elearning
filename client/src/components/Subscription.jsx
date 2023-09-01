bd3d835e3c43151d013a0013373ffa95cb30c68a
Redietberhane <rberhane383@gmail.com>
2023-09-01 13:47:22 +0300

    Update Subscription.jsx

--- client/src/components/Subscription.jsx
+++ client/src/components/Subscription.jsx
@@ -18,7 +18,7 @@ function Subscription() {
   ];
 
   return (
-    <div className="mx-8 rounded-3xl relative isolate overflow-hidden bg-[#0A033C] py-16 sm:py-24 lg:py-32 flex justify-center ">
+    <div className="mx-8 rounded-3xl relative isolate overflow-hidden gradient-purple py-16 sm:py-24 lg:py-32 flex justify-center ">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
           <div

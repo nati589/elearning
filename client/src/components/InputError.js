3031548a2980ef669a91ef9f9f99c2fbae844369
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-04 11:31:51 +0300

    signup validation

---
+++ client/src/components/InputError.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+import {BiErrorCircle} from 'react-icons/bi'
+
+function InputError({message}) {
+    const framer_error = {
+        initial: { opacity: 0, y: 10 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: 10 },
+        transition: { duration: 0.5 },
+      }
+  return (
+   
+          <motion.p
+            className=" flex flex-row flex-nowrap justify-start items-center px-2 py-1 mb-2 h-fit w-fit text-red-700  text-xs lg:text-sm md:text-sm bg-red-300 rounded-md "
+            {...framer_error}
+          >
+            <BiErrorCircle className=' text-red-700'/>
+            &nbsp;
+            {message}
+          </motion.p>
+        )
+      
+}
+
+export default InputError

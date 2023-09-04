4927cc939ae2430ce7c58cf01f0e1d95af14e008
selamawitsiferh <siferhselamawit@gmail.com>
2023-09-04 13:16:29 +0300

    I edited PopularCourceCard files

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -9,7 +9,7 @@ function TestPage() {
       <h1 className=" font-bold tracking-tight text-[#0A033C] sm:text-6xl lg:text-start sm:text-center">Welcome to test page</h1>
 
       {/* <CourseCard course_id="1" /> */}
- <PopularCourseCard course_title='interaction' course_details='orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+ <PopularCourseCard course_title='interaction' course_details='lorem Ipsum is simply dummy text of the printing and typesetting industry. '
       course_thumbnail={selam}  course_total_hour='3Hours'   course_instructure='selam' course_level='level'  course_rate='4.7'  course_price='price'/>
       {/* <PopularCourseCard course_title='interaction' course_details='orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book'
       course_thumbnail={selam}  course_total_hour='3Hours'   course_instructure='selam' course_level='level'  course_rate='4.7'  course_price='price'/>
@@ -19,8 +19,9 @@ function TestPage() {
       course_thumbnail={selam}  course_total_hour='3Hours'   course_instructure='selam' course_level='level'  course_rate='4.7'  course_price='price'/>
       <PopularCourseCard course_title='interaction' course_details='orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book'
       course_thumbnail={selam}  course_total_hour='3Hours'   course_instructure='selam' course_level='level'  course_rate='4.7'  course_price='price'/> */}
-<TrendingCard course_title='interaction' course_details='orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+<TrendingCard course_title='interaction' course_details='lorem Ipsum is simply dummy text of the printing and typesetting industry.'
       course_thumbnail={selam}/> 
+      
 
      
     

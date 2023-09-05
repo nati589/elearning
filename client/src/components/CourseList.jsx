68ca97374daf48dd6b32e4b693fb7bfc3efe33f5
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-06 01:52:45 +0300

    Update CourseList.jsx

--- client/src/components/CourseList.jsx
+++ client/src/components/CourseList.jsx
@@ -27,30 +27,6 @@ function CourseList() {
         "Standard 4 of the Aged Care Quality Standards focuses on services and supports...",
       image: require("../assets/number-4.png"),
     },
-    {
-      title: "Standard Five",
-      description:
-        "Standard 5 Learning Resources. Learning Resources ensure that the school has the...",
-      image: require("../assets/number-5.png"),
-    },
-    {
-      title: "Standard Six",
-      description:
-        "Standard 6 requires an organization to have a system to resolve complaints...",
-      image: require("../assets/number-6.png"),
-    },
-    {
-      title: "Standard Seven",
-      description:
-        "Standard 7 Blood Management mandates that leaders of health service organizations...",
-      image: require("../assets/number-7.png"),
-    },
-    {
-      title: "Standard Eight",
-      description:
-        "Standard 8 Course from NCERT Solutions help students to understand...",
-      image: require("../assets/number-8.png"),
-    },
   ];
 
   return (

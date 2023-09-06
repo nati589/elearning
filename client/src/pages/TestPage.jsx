f83fce2c0bfde8d819726e53cc379b8643bc4105
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-06 03:11:37 +0300

    Update TestPage.jsx

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -21,10 +21,8 @@
 //       course_thumbnail={selam}  course_total_hour='3Hours'   course_instructure='selam' course_level='level'  course_rate='4.7'  course_price='price'/> */}
 // <TrendingCard course_title='interaction' course_details='lorem Ipsum is simply dummy text of the printing and typesetting industry.
 // '
-//       course_thumbnail={selam}/> 
+//       course_thumbnail={selam}/>
 
-     
-    
 //     </div>
 //   );
 // }
@@ -33,14 +31,18 @@
 
 import React from "react";
 import ProgressCard from "../components/ProgressCard";
- function ProgressPage() {
-    const assignArray=[5, 4, 10, 8]
-    const quizArray=[5, 4, 10, 8]
+function ProgressPage() {
+  const assignArray = [5, 4, 10, 8];
+  const quizArray = [5, 4, 10, 8];
 
-    return (
-        <ProgressCard assignment={assignArray} quiz={quizArray} mid='20' final='45' totalCourseContents='30'/>
-    );
-    
-  }
-  export default ProgressPage;
-  
+  return (
+    <ProgressCard
+      assignment={assignArray}
+      quiz={quizArray}
+      mid="20"
+      final="45"
+      totalCourseContents="30"
+    />
+  );
+}
+export default ProgressPage;

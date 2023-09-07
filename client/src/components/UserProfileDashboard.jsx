cab94fc844ca11554b96f0f8bfca55871b8b445d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-08 01:33:02 +0300

    Update UserProfileDashboard.jsx

--- client/src/components/UserProfileDashboard.jsx
+++ client/src/components/UserProfileDashboard.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Profile from "../components/Profile";
-import PopularCourseCard from "../components/PopularCourseCard";
-import selam from "../../src/assets/Cardphoto10.png";
 import Dashboard from "./Dashboard";
 
 function UserProfileDashboard() {
   return (
     <div className="w-full h-full flex flex-col flex-nowrap  ">
-      <div class="bg-white px-5">
-        <div class="flex flex-col flex-nowrap my-3 bg-light-purple rounded-lg shadow-md py-5 w-full justify-center items-center ">
-          <h2 class="text-center  text-subscribe-purple text-2xl font-semibold ">
+      <div className="bg-white px-5">
+        <div className="flex flex-col flex-nowrap my-3 bg-light-purple rounded-lg shadow-md py-5 w-full justify-center items-center ">
+          <h2 className="text-center  text-subscribe-purple text-2xl font-semibold ">
             Enrolled Courses
           </h2>
         </div>

e64b430c6347c5795f16877fb5adab339b1c8236
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-07 11:52:21 +0300

    Update UserProfileDashboard.jsx

--- client/src/components/UserProfileDashboard.jsx
+++ client/src/components/UserProfileDashboard.jsx
@@ -6,11 +6,11 @@ import Dashboard from "./Dashboard";
 
 function UserProfileDashboard() {
   return (
-    <div className="w-full flex flex-col flex-nowrap  ">
-      <div class="bg-white  px-5">
+    <div className="w-full h-full flex flex-col flex-nowrap  ">
+      <div class="bg-white px-5">
         <div class="flex flex-col flex-nowrap my-3 bg-light-purple rounded-lg shadow-md py-5 w-full justify-center items-center ">
           <h2 class="text-center  text-subscribe-purple text-2xl font-semibold ">
-            Your Courses
+            Enrolled Courses
           </h2>
         </div>
       </div>

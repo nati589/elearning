3b40889221408abada0f070b7b8568ea0ab02f85
Ahmed Sufiyan <ahsu891@gmail.com>
2023-09-07 22:29:51 +0300

    add the Month statices

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,6 +4,7 @@ import {
   addBook,
   getBooksThisYear,
   getBooksThisWeek,
+  getBooksThisMonth,
 } from "../controllers/books.js";
 import multer from "multer";
 const upload = multer({ dest: "books/thumbnails/" });
@@ -13,6 +14,7 @@ const router = express.Router();
 router.get("/getBooks", getBooks);
 router.get("/getBooksThisYear", getBooksThisYear);
 router.get("/getBooksThisWeak", getBooksThisWeek);
+router.get("/getBooksThisMonth", getBooksThisMonth);
 // router.put('/updateMember', updateMember);
 // router.delete('/deleteMember', deleteMember);
 router.post("/addBook", upload.single("book_thumbnail"), addBook);

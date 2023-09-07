6c18458bf5301ec0db3d652d23e51fd5a345133b
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-07 22:58:49 +0300

    Update CoursesBody.jsx

--- client/src/components/CoursesBody.jsx
+++ client/src/components/CoursesBody.jsx
@@ -1,4 +1,4 @@
-import { React ,useState} from "react";
+import { React, useState } from "react";
 import CourseCard from "./CourseCard";
 import PopularCourseCard from "./PopularCourseCard";
 import TrendingCard from "./TrendingCoursesCard";
@@ -8,10 +8,9 @@ import SearchComponent from "./search";
 import InputWithSuggestion from "./InputWithSuggestion";
 import courseData from "./courseData";
 
-
 function CoursesBody() {
-  const [courses,setCourses]=useState([...courseData])
-  
+  const [courses, setCourses] = useState([...courseData]);
+
   return (
     <div className="flex flex-col md:flex-row flex-nowrap w-full">
       <div className="w-full md:w-4/12 text-xs md:text-sm px-8 md:px-2 pt-2 my-4 flex flex-col justify-start items-center">
@@ -101,15 +100,13 @@ function CoursesBody() {
           <div className="flex flex-row mx-3 items-center justify-center pl-3 mt-8">
             <label
               htmlFor="sort"
-              className="mr-2 text-lg font-semibold text-purple-700 opacity-50  "
-            >
+              className="mr-2 text-lg font-semibold text-purple-700 opacity-50  ">
               <pre className="sm:text-[1.5rem]">Sort by : </pre>
             </label>
             <select
               name="sort"
               id="sort"
-              className="rounded-md p-2 font-semibold bg-transparent pr-12 text-lg "
-            >
+              className="rounded-md p-2 font-semibold bg-transparent pr-12 text-lg ">
               <option className="" value="a ">
                 Latest
               </option>

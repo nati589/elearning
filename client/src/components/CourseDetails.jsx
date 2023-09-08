52b4b523a97075801ff3dae563ab219778d55cad
selamawitsiferh <siferhselamawit@gmail.com>
2023-09-08 14:47:56 +0300

    edited course detail and footer

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -135,6 +135,7 @@ function CourseDetails() {
         course_price="price"
       />
     </div>
+ 
   </div>
 </div>
 {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">

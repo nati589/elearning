8e131761c80a4afc3b96b0b056cb5411b74e92b5
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-09 13:24:54 +0300

    params update course details

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -3,6 +3,8 @@ import Pricecard from "../components/Pricecard";
 import BookImage from "../../src/assets/Image.png";
 import PopularCourseCard from "./PopularCourseCard";
 import { useNavigate, useParams } from "react-router-dom";
+import DetailsHeroImage from "../assets/ourgoal-image.png";
+
 import axios from "axios";
 
 function CourseDetails() {
@@ -14,46 +16,78 @@ function CourseDetails() {
   // ];
   const navigate = useNavigate();
   const [courseData, setCourseData] = useState({});
-  const { id: courseID } = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
-    if (courseID === null) {
+    if (id === null) {
       navigate("/");
     } else {
       axios
-        .get("/courses/getSingleCourse", { courseID: courseID })
+        .get("/courses/getSingleCourse", { params: { courseID: id } })
         .then((res) => {
-          setCourseData(res.data[0]);
+          setCourseData({ ...res.data[0] });
         })
         .catch((error) => {
           console.log(error.response.data.message);
           // navigate("/");
         });
     }
-  }, [navigate, courseID]);
+  }, [navigate, id]);
   return (
-    <div className="">
-      <div className="flex justify-between gap-8 mx-8 flex-wrap lg:flex-nowrap my-16">
-        <div className="mx-8">
-          <h3 className="font-extrabold text-4xl text-[#0A033C] mb-4">
+    <div className="w-full">
+      <div className="mx-8 bg-white rounded-3xl px-8 py-2 my-8 ">
+        <div className="">
+          <button
+            className="text-gray-400"
+            onClick={() => {
+              navigate("  /");
+            }}>
+            Home
+          </button>
+          <span className="text-gray-400"> | </span>
+          <button
+            className="text-fuchsia-700"
+            onClick={() => {
+              navigate("  /courses");
+            }}>
+            Courses
+          </button>
+          <span className="text-gray-400"> | </span>
+          <button
+            className="text-fuchsia-700"
+            onClick={() => {
+              navigate("  /coursedetails");
+            }}>
             Course Details
+          </button>
+        </div>
+        <div className="flex items-center justify-around py-4 flex-wrap-reverse lg:flex-nowrap">
+          <>
+            <span className="flex gap-4">
+              <h2 className="text-2xl md:text-4xl font-bold text-center my-2">
+                <span className="text-[#9C4DF4]">
+                  {courseData.course_title}
+                </span>
+              </h2>
+            </span>
+          </>
+          <img src={DetailsHeroImage} alt="" className="w-96 rounded-2xl" />
+        </div>
+      </div>
+      <div className="w-full flex justify-between gap-8 flex-wrap lg:flex-nowrap my-16">
+        <div className="pl-8 w-8/12">
+          <h3 className="font-extrabold text-3xl text-[#0A033C] mb-4">
+            Overview
           </h3>
           <p className="whitespace-normal md:whitespace-pre-line mb-4 max-w-full">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
-            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
-            accumsan lacus vel facilisis consectetur adipiscing elit. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-            suspendisse ultrices gravida. Risus commodo viverra maecenas
-            accumsan lacus vel facilisis consectetur adipiscing elit.
+            {courseData.course_details}
           </p>
 
-          <h3 className="font-extrabold text-4xl text-left text-[#0A033C] mb-4">
+          <h3 className="font-extrabold text-3xl text-left text-[#0A033C] mb-4">
             Certification
           </h3>
 
-          <p className="whitespace-normal md:whitespace-pre-line mb-4">
+          {/* <p className="whitespace-normal md:whitespace-pre-line mb-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
             ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
@@ -64,7 +98,7 @@ function CourseDetails() {
             accumsan lacus vel facilisis consectetur adipiscing elit.
           </p>
 
-          <h3 className="font-extrabold text-4xl text-left text-[#0A033C] mb-4">
+          <h3 className="font-extrabold text-3xl text-left text-[#0A033C] mb-4">
             Who this course is for
           </h3>
 
@@ -78,7 +112,7 @@ function CourseDetails() {
             suspendisse ultrices gravida. Risus commodo viverra maecenas
             accumsan lacus vel facilisis consectetur adipiscing elit.
           </p>
-          <h3 className="font-extrabold text-4xl text-left text-[#0A033C] mb-4">
+          <h3 className="font-extrabold text-3xl text-left text-[#0A033C] mb-4">
             What you'll learn in this course:
           </h3>
           <ul className="list-none text-2x">
@@ -106,9 +140,9 @@ function CourseDetails() {
               <span className="text-orange-500 text-3xl mr-2">&#9679;</span>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit
             </li>
-          </ul>
+          </ul> */}
         </div>
-        <div className=" w-full">
+        <div className="mx-4 w-4/12">
           <Pricecard
             course_price="$49.00"
             course_instructure="WadeWarren"
@@ -123,7 +157,7 @@ function CourseDetails() {
         </div>
       </div>
 
-      <h3 className="font-extrabold text-4xl t text-[#0A033C] ml-8 p-9">
+      <h3 className="font-extrabold text-3xl t text-[#0A033C] ml-8 p-9">
         Similar Courses
       </h3>
 

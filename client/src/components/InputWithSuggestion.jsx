0310d005a8609982bb889019828c3ec18454a76c
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-09 11:50:48 +0300

    Intergrate with Api and design improvement

--- client/src/components/InputWithSuggestion.jsx
+++ client/src/components/InputWithSuggestion.jsx
@@ -45,19 +45,19 @@ const InputWithSuggestion = ({ searchData }) => {
       searchResults.length > 0 ? searchResults : searchData;
 
     return (
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 my-8">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-8">
         {coursesToRender.map((course, index) => (
           <CourseCard
             key={index}
             courseId={course.course_id}
-            course_name={course.course_name}
-            course_description="Minim pariatur exercitation est aliquip deserunt id sit tempor voluptate."
+            course_name={course.course_title}
+            course_description={course.course_details}
             courseImagePath="./Image.png"
-            hoursNeeded="2 hours"
-            teacherName="Abe Kebe"
-            rating="3.7"
-            price="40 ETB"
-            level="3"
+            hoursNeeded={course.course_total_hour}
+            teacherName={course.course_instructor}
+            rating={course.course_rating}
+            price={course.course_price}
+            level={course.course_level}
           />
         ))}
       </div>
@@ -66,12 +66,12 @@ const InputWithSuggestion = ({ searchData }) => {
 
   return (
     <div>
-      <div className="flex sm:h-20 h-10 flex-row justify-between mx-3 basis-1/2 my-2 sm:mt-6">
-        <div className="relative ">
+      <div className=" w-full flex h-fit  flex-row justify-between items-center mx-3 basis-1/2 my-3 ">
+        <div className="relative w-full">
           <input
             type="text"
             placeholder="Search title"
-            className="w-full rounded-lg bg-transparent p-2 text-lg font-light pr-4 border-2 border-purple md:pr-28 sm:w-fit"
+            className="w-full rounded-lg bg-transparent p-2 py-3 text-base font-normal border-2 border-purple "
             value={inputValue}
             onChange={handleInputChange}
             onBlur={() => setShowModal(false)}
@@ -83,8 +83,7 @@ const InputWithSuggestion = ({ searchData }) => {
                   <li
                     key={index}
                     className="p-2 cursor-pointer hover:bg-gray-200"
-                    onClick={() => handleSuggestionClick(suggestion)}
-                  >
+                    onClick={() => handleSuggestionClick(suggestion)}>
                     {suggestion.course_name}
                   </li>
                 ))}
@@ -94,12 +93,25 @@ const InputWithSuggestion = ({ searchData }) => {
         </div>
 
         <button
-          className="ml-1 h-fit rounded-lg bg-dark-purple px-8 py-3 text-white hover:bg-blue-600 flex flex-row text-[1rem] sm:px-16 sm:py-2 "
-          onClick={handleOnclick}
-        >
-          <BiSearch className="mr-3 text-[1.5rem]" />
-          <div className="my-auto sm:my-1">Search</div>
+          className="flex flex-row items-center justify-between ml-1  px-6 py-3 h-fit w-fit rounded-lg bg-dark-purple text-white hover:bg-medium-purple text-base "
+          onClick={handleOnclick}>
+          <BiSearch className="text-xl" />
+          <div className="mx-2">Search</div>
         </button>
+
+        <label
+          htmlFor="sort"
+          className=" w-full font-semibold text-base text-purple-700 opacity-50 flex flex-row mx-2 items-center justify-center  ">
+          <span className="text-lg">Sort by :</span>
+          <select
+            name="sort"
+            id="sort"
+            className="rounded-md p-2 bg-transparent text-writing-dark pr-12 ">
+            <option value="a">Latest</option>
+            <option value="b">Oldest</option>
+            <option value="c">c</option>
+          </select>
+        </label>
       </div>
 
       {renderCourses()}

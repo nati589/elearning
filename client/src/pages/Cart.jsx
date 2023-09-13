0cdd7d5037797cdab70e8fe3ebd51590a6d19acc
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-09-14 02:39:15 +0300

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,64 +1,90 @@
 import { useNavigate } from "react-router-dom";
-import CartItem from "../components/CartItem";
+import CartItemCourse from "../components/CartItemCourse";
 import { BiArrowBack } from "react-icons/bi";
+import { useEffect, useState } from "react";
+import CartImg from "../assets/empty_cart.svg";
+import axios from "axios";
+import CartItemBook from "../components/CartItemBook";
 function Cart() {
   const navigate = useNavigate();
+  const [cartEmpty, setCartEmpty] = useState(false);
+  const [cartData, setCartData] = useState([]);
+  const [courseCartData, setCourseCartData] = useState([]);
+  const [bookCartData, setBookCartData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post("/cart/getCart", { user_id: localStorage.getItem("user_id") })
+      .then((response) => {
+        setCartEmpty(response.data.empty);
+        console.log(response.data);
+        // setCartData([...response.data.cart]);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  });
   return (
-    <div className="pt-10 pb-2 lg:px-auto lg:px-32 sm:px-2">
-      <div className="flex flex-row justify-between items-center ">
+    <div className="w-full flex flex-col flex-nowrap pt-10 pb-2 lg:px-auto lg:px-32 sm:px-2">
+      <div className="w-full flex flex-row justify-between items-center ">
         <h1 className="text-2xl md:text-3xl">Shopping cart</h1>
         <button
           onClick={() => navigate(-1)}
-          className="bg-purple-30 gap-1 flex items-center p-1 text-sm md:text-md rounded text-medium-purple"
-        >
+          className="bg-purple-30 gap-1 flex items-center p-1 text-sm md:text-md rounded text-medium-purple">
           <BiArrowBack className="text-lg" />
           Continue shopping
         </button>
       </div>
-      <div className="mt-16">
-        <h2>you have 3 items in your cart</h2>
-      </div>
-      <div className=" flex flex-col gap-2 mb-4  py-1 max-w-max">
-        <CartItem
-          title="Advance amharic course part one"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit ipsa dicta, nesciunt error perspiciatis repellendus neque aut deleniti quidem veniam porro, voluptatum"
-          hour="31"
-          level="Intermadate"
-          lecture="32"
-          rate="4.7"
-          price={16.5}
-        />
-        <CartItem
-          title="Advance amharic course part one"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit ipsa dicta, nesciunt error perspiciatis repellendus neque aut deleniti quidem veniam porro, voluptatum"
-          hour="31"
-          level="Intermadate"
-          lecture="32"
-          rate="4.7"
-          price={16.5}
-        />
-        <CartItem
-          title="Advance amharic course part one"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit ipsa dicta, nesciunt error perspiciatis repellendus neque aut deleniti quidem veniam porro, voluptatum"
-          hour="31"
-          level="Intermadate"
-          lecture="32"
-          rate="4.7"
-          price={16.5}
-        />
-
-        <div className="flex flex-col gap-1 self-end mt-3">
-          <div className=" bg-purple-100 w-40 p-2 text-xl rounded flex flex-row justify-between">
-            <span>Total</span>
-            <span className="text-medium-purple">$32.12</span>
-          </div>
-          <div>
-            <button className="bg-medium-purple text-white p-2 rounded  w-40">
-              Checkout
-            </button>
-          </div>
+      {cartEmpty ? (
+        <div className="w-full flex flex-col flex-nowrap text-center text-3xl font-light mt-12 ">
+          <h1>Your Cart is empty</h1>
+          <img
+            src={CartImg}
+            alt="empty cart"
+            className="w-4/12 h-auto m-auto mb-3 mt-3"
+          />
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="w-full mt-16">
+            <h2>you have {cartData.length} items in your cart</h2>
+          </div>
+          {}
+          <div className="w-full flex flex-col gap-2 mb-4  py-1 max-w-max">
+            <h2>Books</h2>
+
+            {/* {bookCartData.map((item, index) => (
+              <CartItemBook
+                key={item.cart_iddex}
+                cart_id={item.cart_id}
+                book_id={item.course_id}
+              />
+            ))} */}
+
+            <h2>Courses</h2>
+
+            {/* {courseCartData.map((item, index) => (
+              <CartItemCourse
+                key={item.cart_id}
+                cart_id={item.cart_id}
+                course_id={item.course_id}
+              />
+            ))} */}
+
+            <div className="flex flex-col gap-1 self-end mt-3">
+              <div className=" bg-purple-100 w-40 p-2 text-xl rounded flex flex-row justify-between">
+                <span>Total</span>
+                <span className="text-medium-purple">$32.12</span>
+              </div>
+              <div>
+                <button className="bg-medium-purple text-white p-2 rounded  w-40">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }

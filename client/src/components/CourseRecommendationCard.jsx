a3b6959228a259138d0169fe1f88b7be3f859cf4
Rediettt <64363596+Rediettt@users.noreply.github.com>
2023-09-17 15:05:41 +0300

    Course Recommendation Update

--- client/src/components/CourseRecommendationCard.jsx
+++ client/src/components/CourseRecommendationCard.jsx
@@ -1,153 +1,96 @@
-import React, { useEffect, useState } from "react";
-import dollar from "../assets/icons/dollar.svg";
-import person from "../assets/icons/person.svg";
-import timer from "../assets/icons/timer.svg";
-import FullStar from "../assets/icons/full_star.svg";
-import EmptyStar from "../assets/icons/empty_star.svg";
-import stickynote from "../assets/icons/stickynote.svg";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import ResponseMessage from "./ResponseMessage";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import RecommendedCourseImg from "../assets/RecommendedCourseImg.png"
 
-
-const BookRecommendationCard = ({
-  courseId,
-  course_title,
-  course_details,
-  course_thumbnail,
-  course_total_hour,
-  course_instructure,
-  course_level,
-  course_rate,
-  course_price,
-}) => {
-  // const images = require.context("../assets");
-
-  // const course_image = images(course_thumbnail);
-  const numbers = [1, 2, 3, 4, 5];
-  const partialDesc = course_details ? course_details.slice(0, 80) : "";
-
-  const [failure, setFailure] = useState(false);
-  const [addSuccess, setAddSuccess] = useState(false);
-  const [resMsg, setResMsg] = useState("");
-
-  useEffect(() => {
-    if (localStorage.getItem("user_id")) {
-      axios
-        .post("/cart/checkCourse", {
-          course_id: courseId,
-          user_id: localStorage.getItem("user_id"),
-        })
-        .then((res) => {
-          if (res.data.message) {
-            setAddSuccess(true);
-            setFailure(false);
-            setResMsg("Already in cart");
-          }
-        })
-        .catch((error) => {
-          console.log(error.response.data.message);
-        });
-    }
-  }, [courseId]);
-
-  const addToCart = () => {
-    axios
-      .post("/cart/addToCart", {
-        course_id: courseId,
-        user_id: localStorage.getItem("user_id"),
-        book_id: null,
-      })
-      .then((res) => {
-        setAddSuccess(true);
-        setFailure(false);
-        setResMsg(res.data.message);
-      })
-      .catch((error) => {
-        setResMsg(error.response.data.message);
-        setAddSuccess(true);
-        setFailure(true);
-      });
-  };
+function CourseRecommendationCard() {
   return (
-    <div className="card bg-white border-l-8 border-l-dark-purple rounded-lg shadow-md my-4 p-6 w-80 h-88">
-      <Link to={`/bookdetails/${courseId}`} className=" cursor-pointer">
-        <h1 className="text-xl mb-4 font-bold">{course_title}</h1>
-        <div className="grid grid-cols-4 gap-4">
-          <div className="col-span-1">
-            <img className="w-32 h-12 " src={course_thumbnail} alt="books" />
-          </div>
-          <div className="col-span-3">
-            <p className="text-sm">{course_details}</p>
-          </div>
-        </div>
-
-        <div className="flex flex-row m-2">
-          <div className="flex-grow">
-            <div className="grid grid-rows-3 ">
-              <div className="flex flex-row">
-                <img className="w-4 h-4" src={timer} alt="" />
-                <p className="ml-2 text-sm">{course_total_hour}</p>
-              </div>
-              <div className="flex flex-row">
-                <img className="w-4 h-4" src={person} alt="" />
-                <p className="ml-2 text-sm">{course_instructure}</p>
-              </div>
-              <div className="flex flex-row">
-                <img className="w-4 h-4" src={stickynote} alt="" />
-                <p className="ml-2 text-sm">{course_level}</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex-grow">
-            <div className="grid grid-rows-3 ">
-              <div className="flex flex-row">
-                <p className="ml-2 text-sm">{course_rate}</p>
-                {numbers.map((number) => {
-                  const chosenIcon =
-                    number <= Math.floor(course_rate) ? FullStar : EmptyStar;
-                  return (
-                    <img
-                      key={number}
-                      src={chosenIcon}
-                      alt="empty star icon"
-                      className="w-4 h-4 ml-0.5"
-                    />
-                  );
-                })}
-              </div>
-              <div className="flex flex-row">
-                <img className="w-4 h-4" src={dollar} alt="" />
-                <p className="ml-2 text-sm">{course_price}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link>
-      <div className="flex flex-row">
-        <div className="flex flex-col w-full justify-center items-center mt-4">
-          {addSuccess && <ResponseMessage failure={failure} message={resMsg} />}
-
-          {(!addSuccess || failure) &&
-            localStorage.getItem("username") &&
-            localStorage.getItem("user_id") && (
-              <button
-                className="button-component-stroke w-fit py-2 px-8 mb-2"
-                onClick={addToCart}
+    <div>
+      <div className="font-bold">
+        <div className="flex flex-wrap my-4">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col bg-white my-2">
+              <a
+                href="/bookdetails"
+                className="w-full border-2 border-b-4 border-gray-200 rounded-xl hover:bg-gray-50"
               >
-                <span className="add-to-cart-text">Add to Cart</span>{" "}
-                &nbsp;&nbsp; &nbsp;
-                <span className="cart-icon w-4 h-4">
-                  <FontAwesomeIcon icon={faShoppingCart} />
-                </span>
-              </button>
-            )}
+                <div className="grid grid-cols-6 p-5 gap-y-2">
+                  {/* <!-- Profile Picture --> */}
+                  <div>
+                    <img
+                      src={RecommendedCourseImg}
+                      className="max-w-16 max-h-16"
+                      alt="kd"
+                    />
+                  </div>
+
+                  {/* <!-- Description --> */}
+                  <div className="col-span-5 md:col-span-4 ml-4">
+                    <p className="text-gray-600 font-bold">
+                      {" "}
+                      [Beginner] Light Discussion{" "}
+                    </p>
+
+                    <div className="flex items-center w-16">
+                      <svg
+                        className="w-5 h-5 text-yellow-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        className="w-5 h-5 text-yellow-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        className="w-5 h-5 text-yellow-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        className="w-5 h-5 text-yellow-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* <!-- Price --> */}
+                
+                </div>
+                <div className="flex col-start-2 ml-4 md:col-start-auto md:ml-0 md:justify-center mb-2">
+                    <p className="rounded-lg text-dark-purple font-bold bg-light-purple  py-1 px-3 text-sm w-fit h-fit">
+                      {" "}
+                      Add to cart{" "}
+                    </p>
+                  </div>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+   
+      </div>
+    
   );
-};
+}
 
-export default BookRecommendationCard;
+export default CourseRecommendationCard;
